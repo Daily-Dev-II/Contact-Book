680d9d8a81633e805a270a33c55f38dc5eb742fd
Abongwa Bonalais Amahnui <abongwabonalais@gmail.com>
2022-05-31 16:05:08 +0100

    configure _createcontactstable

--- rpcontacts/database.py
+++ rpcontacts/database.py
@@ -1,5 +1,22 @@
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+
+
+def _createContactsTable():
+    """Create the contacts table in the database."""
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            job VARCHAR(50),
+            email VARCHAR(40) NOT NULL
+        )
+        """
+    )
 
 def createConnection(databaseName):
     """Create and open a database connection."""
@@ -14,4 +31,5 @@ def createConnection(databaseName):
         )
         return False
 
+    _createContactsTable()
     return True
